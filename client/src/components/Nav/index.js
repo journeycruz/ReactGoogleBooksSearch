fe22f33afc592ce3bfbb56843a8fbe7aafc59c1f
Journey Cruz <journey.cruz@gmail.com>
2021-06-28 12:43:26 -0500

    add two links into navbar for search and saved books

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,9 +3,15 @@ import React from "react";
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
-      </a>
+      <a className="navbar-brand" href="/">React Reading List Powered by Google Books</a>
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className={"nav-link" + ((pathname === "/search" || pathname === "/") ? " active" : "")} href="/search">Search</a>
+        </li>
+        <li className="nav-item">
+          <a className={"nav-link" + ((pathname === "/books") ? " active":"")} href="/books">Saved Books</a>
+        </li>
+      </ul>
     </nav>
   );
 }
