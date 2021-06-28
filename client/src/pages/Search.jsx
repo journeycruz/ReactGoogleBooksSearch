3f796767284dc6dadc14209ab4e144cc145b77c4
Journey Cruz <journey.cruz@gmail.com>
2021-06-28 12:30:15 -0500

    Create Book Component

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,19 +1,1 @@
 import React, { useState } from "react";
-
-export default Search()
-const [book, setBook] = useState({});
-const [formInput, setFormInput] = useState({});
-
-function newBook(i) {
-    var book = 
-}
-return (
-    <div>
-        <form>
-            <input
-                name="bookTitle"
-                placeholder="title"
-            />
-        </form>
-    </div>
-)
